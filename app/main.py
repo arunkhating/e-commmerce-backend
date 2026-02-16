1b4f4f87c62b07e96ad25411197bb48ae7b7e294
Arun khating <140695960+arunkhating@users.noreply.github.com>
2026-02-16 16:27:27 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 # from fastapi import FastAPI
 from app.database import Base, engine
+# from app.routes import auth, products
 from app.routes import auth, products
 
 Base.metadata.create_all(bind=engine)
